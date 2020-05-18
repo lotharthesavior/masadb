b2a5d06c04ea6cec177f701f8fb5030ff543a911
Savio Resende <savio@savioresende.com.br>
2020-05-18 08:38:08 -0400

    Masadb running on swoole.

--- app/oauth2.php
+++ app/oauth2.php
@@ -3,6 +3,8 @@
 // Init our repositories
 $accessTokenRepository = new \Repositories\AccessTokenRepository(); // instance of AccessTokenRepositoryInterface
 
+// clearstatcache(); // this might be needed to avoid issues with certificates permissions when configuring the db
+
 // Path to authorization server's public key
 $publicKey = $config['settings']['public_key'];
       
--- index.php
+++ index.php
@@ -13,6 +13,8 @@ require __DIR__ . '/vendor/autoload.php';
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Pachico\SlimSwoole\BridgeManager;
+use Slim\Http;
 
 // Load configuration
 $config_json = file_get_contents("config.json");
@@ -32,4 +34,32 @@ include "app/controllers.php";
 
 include "routes.php";
 
-$app->run();
+// $app->run();
+
+$bridgeManager = new BridgeManager($app);
+
+/**
+ * We start the Swoole server
+ */
+$http = new swoole_http_server("0.0.0.0", 80);
+
+/**
+ * We register the on "start" event
+ */
+$http->on("start", function (\swoole_http_server $server) {
+    echo sprintf('Swoole http server is started at http://%s:%s', $server->host, $server->port), PHP_EOL;
+});
+
+/**
+ * We register the on "request event, which will use the BridgeManager to transform request, process it
+ * as a Slim request and merge back the response
+ *
+ */
+$http->on(
+    "request",
+    function (swoole_http_request $swooleRequest, swoole_http_response $swooleResponse) use ($bridgeManager) {
+        $bridgeManager->process($swooleRequest, $swooleResponse)->end();
+    }
+);
+
+$http->start();
