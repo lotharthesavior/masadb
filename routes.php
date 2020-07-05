1b70053816d77fe0219c58c58d74d310a2b553f6
Savio Resende <savio@savioresende.com.br>
2020-07-05 12:36:59 -0400

    Added todos to routes files.

--- routes.php
+++ routes.php
@@ -5,16 +5,22 @@ use \League\OAuth2\Server\Middleware\ResourceServerMiddleware;
 $app->get('/', 'HomeController:home');
 
 
-// OAUTH2 ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+// OAUTH2 
+// ------------------------------------------------------------------------------------------
 
 $app->post('/access_token', 'OAuthController:accessToken');
 
 $app->post('/generate_key', 'OAuthController:generateClientKey');
 
-// / OAUTH2 ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+// / OAUTH2 
+// ----------------------------------------------------------------------------------------
 
 
-// ASYNC CALLS ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+// ASYNC CALLS 
+// ----------------------------------------------------------------------------------------
 
 // $app->post('/git-async', 'MasaDBController:gitAsync');
 //     ->add(new ResourceServerMiddleware($server));
@@ -22,10 +28,15 @@ $app->post('/generate_key', 'OAuthController:generateClientKey');
 // $app->post('/update-cache-async', 'MasaDBController:updateCacheAsync');
 //     ->add(new ResourceServerMiddleware($server));
 
-// / ASYNC CALLS ----------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------
+// / ASYNC CALLS 
+// ----------------------------------------------------------------------------------------
 
 
-// Generic Database ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+// Generic Database 
+// ------------------------------------------------------------------------------------------
 
 $oauthMiddleware = function ($request, $response, $next) use ($config, $server) {
     if ($config['settings']['env'] === APP_ENV_PROD) {
@@ -65,5 +76,87 @@ $app->delete('/{database}/{id:[0-9]+}', 'MasaDBController:deleteGeneric')
 $app->delete('/{database}/file', 'MasaDBController:deleteGenericFile')
     ->add($oauthMiddleware);
 
-// / Generic Database ------------------------------------------------------------------------------------------
+// TODO: upload asset
 
+// ------------------------------------------------------------------------------------------
+// / Generic Database 
+// ------------------------------------------------------------------------------------------
+
+
+// ------------------------------------------------------------------------------------------
+// Git Procedures
+// ------------------------------------------------------------------------------------------
+
+// ----COMMITS----
+
+// TODO: Stage Commit
+
+// TODO: Create Commit
+
+// TODO: Revert Commit
+
+// TODO: Amend Commit
+
+// ----LOGS---
+
+// TODO: Show X Logs
+
+// TODO: Search Logs
+
+// ----REMOTES----
+
+// TODO: Add Remote
+
+// TODO: Remove Remote
+
+// TODO: List Remote
+
+// TODO: Find Remote
+
+// ----BRANCHES----
+
+// TODO: Add Branch
+
+// TODO: Remove Branch
+
+// TODO: Remove Remote Branch
+
+// TODO: Push to Remote Branch
+
+// TODO: Pull from Remote Branch
+
+// TODO: Move to Branch (checkout)
+
+// TODO: Merge Branch
+
+// TODO: Get Current Branch
+
+// TODO: Find Branch
+
+// ----TAGS----
+
+// TODO: List Tags
+
+// TODO: Search Tags
+
+// TODO: Create Tags
+
+// TODO: Show Tags
+
+// TODO: Add Tag to Commit
+
+// TODO: Push Tag to Remote
+
+// TODO: Move to Tag (checkout)
+
+// ----CONFIGS----
+
+// TODO: set automatic commits: false or true
+
+// TODO: set automatic push remotes: false or true
+
+// TODO: set to ignore chmod changes: false or true
+
+// ------------------------------------------------------------------------------------------
+// / Git Procedures
+// ------------------------------------------------------------------------------------------
